f924f999eef04f03b598a46ce8b4739b6153ab0f
onedrako <onedrako@gmail.com>
2022-04-28 14:33:41 -0500

    Added front 4

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('
 
 app.use(express.json())
 
-const whiteList = [config.frontendUrl, config.localhost, 'https://thewintergallery.vercel.app/', 'https://thewintergallery-7t4c9vwbg-onedrako.vercel.app/']
+const whiteList = ['https://thewintergallery.vercel.app/', 'https://thewintergallery-7t4c9vwbg-onedrako.vercel.app/']
 
 const corsOptions = {
   origin: (origin, callback) => {
