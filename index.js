41454ef719125ca816dc4528cb17b267d352733c
onedrako <onedrako@gmail.com>
2022-04-28 14:38:38 -0500

    Added front 5

--- index.js
+++ index.js
@@ -5,25 +5,24 @@ const { config } = require('./config/config')
 const routerApi = require('./routes')
 
 const app = express()
-const port = config.port
+const port = config.port || 3000
 
 const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler')
 
 app.use(express.json())
 
-const whiteList = ['https://thewintergallery.vercel.app/', 'https://thewintergallery-7t4c9vwbg-onedrako.vercel.app/']
+const whitelist = [config.frontendUrl, config.localhost, 'https://thewintergallery.vercel.app/', 'https://thewintergallery-7t4c9vwbg-onedrako.vercel.app/']
 
-const corsOptions = {
+const options = {
   origin: (origin, callback) => {
-    if (whiteList.includes(origin)) {
+    if (whitelist.includes(origin) || !origin) {
       callback(null, true)
     } else {
-      callback(new Error('Not estas autorizado'))
+      callback(new Error('No estas autorizado'))
     }
   }
 }
-
-app.use(cors(corsOptions))
+app.use(cors(options))
 
 require('./utils/auth')
 app.use(passport.initialize())
